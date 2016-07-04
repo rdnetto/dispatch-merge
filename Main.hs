fb67b577fcbad9be34d5aa14bddf9e595fa02a1f
Reuben D'Netto <rdnetto@atlassian.com>
2016-07-04 12:32:55 +1000

    Display output

--- Main.hs
+++ Main.hs
@@ -4,23 +4,23 @@ import Data.List
 main :: IO ()
 main = do
     txt <- readFile "test.txt"
-    let contents = parseText txt
-    return ()
+    print $ parseText txt
 
 
 data DiffSection = HText [String]
                     | HConflict Hunk Hunk
+                    deriving Show
 data Hunk = Hunk {
     name :: String,
     contents :: [String]
-}
+} deriving Show
 
 parseText :: String -> [DiffSection]
 parseText txt = HText startText : parseHunk hunk
     where
         (startText, hunk) = break (isPrefixOf conflict_start_marker) $ lines txt
 
-parseHunk hunk = undefined
+parseHunk hunk = []
 
 conflict_start_marker = "<<<<<<< "
 conflict_sep_marker = "======="
