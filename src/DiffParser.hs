3bcfe7081662531c9b8792fa35869a5f8a795cc3
Reuben D'Netto <rdnetto@atlassian.com>
2016-08-09 17:06:18 +1000

    Added support for diff3 conflict style.
    Fixes #1.

--- src/DiffParser.hs
+++ src/DiffParser.hs
@@ -20,8 +20,9 @@ data Hunk = Hunk {
 } deriving (Show, Eq)
 
 
-conflict_start_marker, conflict_sep_marker, conflict_end_marker :: String
+conflict_start_marker, conflict_com_marker, conflict_sep_marker, conflict_end_marker :: String
 conflict_start_marker = "<<<<<<< "
+conflict_com_marker = "||||||| "
 conflict_sep_marker = "======="
 conflict_end_marker = ">>>>>>> "
 
@@ -55,15 +56,39 @@ parseHunk _ [] = []
 parseHunk (lLn, rLn) lns =
     let
         header:xs = lns
-        (hunk1, _:ys) = break (== conflict_sep_marker) xs
-        (hunk2, footer:rest) = break (isPrefixOf conflict_end_marker) ys
+        (body, footer:rest) = break (isPrefixOf conflict_end_marker) xs
+        (hunk1, hunk2) = parseHunkInternal body
 
         name1 = dropPrefix conflict_start_marker header
         name2 = dropPrefix conflict_end_marker footer
 
         lLn' = lLn + length hunk1
-        rLn' = rLn + length hunk1
+        rLn' = rLn + length hunk2
     in HConflict (Hunk name1 hunk1 lLn) (Hunk name2 hunk2 rLn) : parseTextInternal (lLn', rLn') rest
+
+-- Parses the body of a merge conflict into two hunks.
+-- Merge conflicts can be in 'merge' style (default) or 'diff3' style
+--
+{- Section under consideration:
+ - <hunk1>
+ - |||||||
+ - <common base>
+ - =======
+ - <hunk2>
+ -}
+parseHunkInternal :: [String] -> ([String], [String])
+parseHunkInternal lns | isDiff3   = (d3_hunk1, d3_hunk2)
+                      | otherwise = (m_hunk1,  m_hunk2)
+    where
+        (a, b) = break (isPrefixOf conflict_com_marker) lns
+        isDiff3 = (b /= [])
+
+        -- merge style
+        (m_hunk1, _:m_hunk2) = break (== conflict_sep_marker) a
+
+        -- diff3 style
+        d3_hunk1 = a
+        (common, _:d3_hunk2) = break (== conflict_sep_marker) b
 
 dropPrefix :: String -> String -> String
 dropPrefix p = drop $ length p
